138e685688415056adf374a5eb024fe93d12a13d
SPEPTW <sebastien@xboxlive.fr>
2019-10-05 18:34:40 +0200

    ref 2 counter wip

--- counter.js
+++ counter.js
@@ -2,3 +2,16 @@
 // count number of clicks
 // count 1 when key of keyboard used
 // show content in a schema
+
+
+var button = document.getElementById("seb");
+var counter = document.getElementById("counter");
+count = 0;
+button.onclick = function() {
+    count += 1;
+    counter.innerHTML = "Total des \"Hey Christophe\" : "+count;
+};
+button.onkeypress = function () {
+    count += 1;
+    counter.innerHTML = "Total des \"hey Christophe\""+count;
+};
